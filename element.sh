c6b6a55cf7c76a54c19483aa8823d19242aeee6e
Awsam <awsam.haitham@outlook.com>
2024-02-10 03:54:40 -0500

    feat: building the structure

--- element.sh
+++ element.sh
@@ -2,6 +2,13 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-echo Please provide an element as an argument.
+SYMBOL=$1
 
+if [[ -z $1 ]]
+then
+  #if the input is not there 
+  echo Please provide an element as an argument.
 
+else 
+  echo hello
+fi
